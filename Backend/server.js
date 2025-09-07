566783ae80bf9f0f9c7eabe9b9e1e207517ff879
bikram3369 <bikramsarkar101010@gmail.com>
2025-09-07 19:39:14 +0530

    7-9-25

---
+++ Backend/server.js
@@ -0,0 +1,47 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+
+// import routes (later you can create them)
+import userRoute from "./routes/user.route.js";
+import companyRoute from "./routes/company.route.js";
+import jobRoute from "./routes/job.route.js";
+import applicationRoute from "./routes/application.route.js";
+
+
+dotenv.config();
+
+// Initialize app
+const app = express();
+
+// Connect to MongoDB
+connectDB();
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+const corsOptions = {
+  origin: "http://localhost:5173", // React frontend
+  credentials: true,
+};
+app.use(cors(corsOptions));
+
+// Routes
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/company", companyRoute);
+app.use("/api/v1/job", jobRoute);
+app.use("/api/v1/application", applicationRoute);
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "🚀 API is running..." });
+});
+
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`✅ Server running at port ${PORT}`);
+});
