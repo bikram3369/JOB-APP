1fb281a72b5c18d4c04de37e8b7e422919333936
Bikram Sarkar <157918024+bikram3369@users.noreply.github.com>
2025-09-09 19:17:52 +0530

    Update constant.js

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,3 +1,3 @@
-export const USER_API_END_POINT="https://codeforge-1uc7.onrender.com/api/v1/user";
+export const USER_API_END_POINT="https://codeforge-1uc7.onrender.com/user";
 // export const USER_API_END_POINT="http://localhost:8000/api/v1/user";
 // https://codeforge-1uc7.onrender.com
