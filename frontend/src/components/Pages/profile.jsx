3db02ea744f19227816f16071cbd17009b6fff6b
bikram3369 <bikramsarkar8240@gmail.com>
2026-01-09 00:46:41 +0530

    profile file upated with correct navbar import

--- frontend/src/components/Pages/profile.jsx
+++ frontend/src/components/Pages/profile.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "@/components/shared/Navbar";
+import Navbar from "@/components/shared/navbar";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import { useSelector } from "react-redux";
 import { Button } from "@/components/ui/button";
