259b3f0d9fc5c2858082416b4416e39d80f99437
bikram3369 <bikramsarkar8240@gmail.com>
2026-01-09 22:55:54 +0530

    route update

--- Backend/server.js
+++ Backend/server.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: "http://localhost:5173", // React frontend
+  origin: "https://codeforces-three.vercel.app", // React frontend
   credentials: true,
 };
 app.use(cors(corsOptions));
