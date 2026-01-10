0ea325f011410c979d5d920d79018d7446225200
bikram3369 <bikramsarkar8240@gmail.com>
2026-01-10 23:28:18 +0530

    constant js update

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -3,7 +3,7 @@
 // export const APPLICATION_API_END_POINT="http://localhost:8000/api/v1/application";
 // export const COMPANY_API_END_POINT="http://localhost:8000/api/v1/company";
 
-const BASE_URL = "https://job-app-khaki.vercel.app";
+const BASE_URL = "https://job-app-swart-seven.vercel.app";
 
 export const USER_API_END_POINT = `${BASE_URL}/api/v1/user`;
 export const JOB_API_END_POINT = `${BASE_URL}/api/v1/job`;
